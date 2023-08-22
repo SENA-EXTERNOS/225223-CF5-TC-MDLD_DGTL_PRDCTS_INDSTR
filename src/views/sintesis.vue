842d62e8d1a39166e4ea99a14e23478c9a78c4fc
Camilo Bolaño <80546125+elrilacho@users.noreply.github.com>
2023-08-22 16:27:13 -0500

    Ajustes_repositorio

--- src/views/sintesis.vue
+++ src/views/sintesis.vue
@@ -3,8 +3,10 @@
   BannerInterno(icono="fas fa-sitemap" titulo="Síntesis")
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5
 
-    p.mb-5 A continuación, se muestra un mapa conceptual con los elementos más importantes desarrollados en este componente.
-
+    p.mb-5 Analizar en la presente síntesis los temas tratados de este componente formativo.
+    .titulo-sexto.color-acento-contenido(data-aos='fade-right')
+      h5 Figura 2
+      span Post procesamiento de datos y nubes de puntos
     .row.justify-content-center
       .col-lg-10.mb-5
         figure
